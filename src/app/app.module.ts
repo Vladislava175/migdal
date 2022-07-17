0c3e0cc64eead3d361c5826ee85e3dc4264e4b85
IL-TLV-WIN0113\vladislava.zhyguliov <vladislava.zhyguliovtsev@allcloud.io>
2022-07-17 16:56:38 +0300

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { ProcessInfoComponent } from './components/process-info/process-info.component';
+import { ContactsInfoComponent } from './components/contacts-info/contacts-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    ProcessInfoComponent,
+    ContactsInfoComponent
   ],
   imports: [
     BrowserModule
